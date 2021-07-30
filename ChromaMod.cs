e7f2a96c2466e3f253790b8aee49368f2877253b
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2021-07-30 23:33:48 +0200

    Added very simple setting file

--- ChromaMod.cs
+++ ChromaMod.cs
@@ -1,8 +1,9 @@
 ﻿using BepInEx;
 using BepInEx.IL2CPP;
 using HarmonyLib;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
+using System.IO;
 
 namespace SpeenChroma
 {
@@ -11,13 +12,24 @@ namespace SpeenChroma
     {
         public const string MOD_ID = "ChromaMod";
         public const string MOD_NAME = "Speen Chroma";
-        public const string MOD_VERSION = "1.0.0";
+        public const string MOD_VERSION = "1.0.1";
+
+        public static string FilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "SpeenChromaSpeed.txt");
 
         public static BepInEx.Logging.ManualLogSource Logger;
 
         public override void Load()
         {
             Logger = BepInEx.Logging.Logger.CreateLogSource(MOD_ID);
+
+            if (!File.Exists(FilePath)) File.Create(FilePath).Close();
+
+            var value = File.ReadAllLines(FilePath);
+            if (value.Length > 0 && double.TryParse(value[0], out double speed) && speed > 0) Patches.RainbowSpeed = (float)speed; // I don't know why that works but I'll take it
+            else Patches.RainbowSpeed = 1f;
+
+            Logger.LogMessage("Rainbow speed set to: " + Patches.RainbowSpeed);
+
             Harmony.CreateAndPatchAll(typeof(ChromaMod.Patches));
         }
 
@@ -41,7 +53,7 @@ namespace SpeenChroma
             {
                 foreach (GameplayColorBlender blender in blenders)
                 {
-                    blender.SetHSL((blender.hue >= 1f ? blender.hue - 1f : blender.hue) + 0.1f * Time.deltaTime, blender.saturation, blender.lightness);
+                    blender.SetHSL((blender.hue >= 1f ? blender.hue - 1f : blender.hue) + 0.1f * Time.deltaTime * RainbowSpeed, blender.saturation, blender.lightness);
                 }
             }
         }
